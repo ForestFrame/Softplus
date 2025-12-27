1e2c8e9461e492442c3f1a115577362e3abd0ad6
ForestFrame <forestframe@163.com>
2025-12-27 19:08:24 +0800

    ✨ feat:
    1.加上了threshold阈值判断，对算子进行了分段处理；
    2.加上了Cast类型转换，解决了类型转换相关的Bug，但是是否起到了作用还存疑；
    测试结果上来看性能略有降低，case3和5的计算结果都是错误，不知道是不是因为类型转换错误造成的。

--- SoftplusCustom/op_kernel/softplus.cpp
+++ SoftplusCustom/op_kernel/softplus.cpp
@@ -4,8 +4,10 @@ using namespace AscendC;
 
 #define PING_PONG_BUFFER_NUM 2
 
+template<typename TYPE_X, typename TYPE_Y>
 class KernelSoftplus
 {
+    using T = TYPE_Y;
 public:
     __aicore__ inline KernelSoftplus() {}
     __aicore__ inline void Init(GM_ADDR x, GM_ADDR y, uint32_t total_data_num,
@@ -26,11 +28,13 @@ public:
         this->beta = beta;
         this->threshold = threshold;
 
-        xGm.SetGlobalBuffer((__gm__ DTYPE_X *)x + this->tilingDataNum * AscendC::GetBlockIdx(), this->tilingDataNum);
-        yGm.SetGlobalBuffer((__gm__ DTYPE_Y *)y + this->tilingDataNum * AscendC::GetBlockIdx(), this->tilingDataNum);
-        pipe.InitBuffer(inQueueX, PING_PONG_BUFFER_NUM, this->tilingDataNum * sizeof(DTYPE_X));
-        pipe.InitBuffer(outQueueY, PING_PONG_BUFFER_NUM, this->tilingDataNum * sizeof(DTYPE_Y));
-        pipe.InitBuffer(calBuf, this->tilingDataNum * sizeof(float));
+        xGm.SetGlobalBuffer((__gm__ TYPE_X *)x + this->tilingDataNum * AscendC::GetBlockIdx(), this->tilingDataNum);
+        yGm.SetGlobalBuffer((__gm__ TYPE_Y *)y + this->tilingDataNum * AscendC::GetBlockIdx(), this->tilingDataNum);
+        pipe.InitBuffer(inQueueX, PING_PONG_BUFFER_NUM, this->tilingDataNum * sizeof(TYPE_X));
+        pipe.InitBuffer(outQueueY, PING_PONG_BUFFER_NUM, this->tilingDataNum * sizeof(TYPE_Y));
+        pipe.InitBuffer(calBuf1, this->tilingDataNum * sizeof(uint8_t));
+        pipe.InitBuffer(calBuf2, this->tilingDataNum * sizeof(float32_t));
+        pipe.InitBuffer(calBuf3, this->tilingDataNum * sizeof(float32_t));
     }
     __aicore__ inline void Process()
     {
@@ -52,7 +56,7 @@ public:
 private:
     __aicore__ inline void CopyIn(int32_t progress, uint32_t dataNum)
     {
-        AscendC::LocalTensor<DTYPE_X> xLocal = inQueueX.AllocTensor<DTYPE_X>();
+        AscendC::LocalTensor<TYPE_X> xLocal = inQueueX.AllocTensor<TYPE_X>();
         AscendC::DataCopy(xLocal, xGm[progress * this->tilingDataNum], dataNum);
         inQueueX.EnQue(xLocal);
     }
@@ -60,32 +64,47 @@ private:
     __aicore__ inline void Compute(int32_t progress, uint32_t dataNum)
     {
         auto scalar = 1;
-        AscendC::LocalTensor<DTYPE_X> xLocal = inQueueX.DeQue<DTYPE_X>();
-        AscendC::LocalTensor<DTYPE_Y> yLocal = outQueueY.AllocTensor<DTYPE_Y>();
-        AscendC::LocalTensor<float> tempTensor = calBuf.Get<float>(dataNum);
+        AscendC::LocalTensor<TYPE_X> xLocal = inQueueX.DeQue<TYPE_X>();
+        AscendC::LocalTensor<TYPE_Y> yLocal = outQueueY.AllocTensor<TYPE_Y>();
+        AscendC::LocalTensor<uint8_t> mask = calBuf1.Get<uint8_t>(dataNum);
 
-        printf("Before Softplus computation, xLocal data:\n");
-        AscendC::DumpTensor(xLocal, 0, 32);
+        if constexpr (std::is_same_v<T, bfloat16_t> || std::is_same_v<T, float16_t>)
+        {
+            auto temp1 = calBuf3.Get<float32_t>(dataNum);
+            auto temp2 = calBuf2.Get<float32_t>(dataNum);
 
-        // Softplus计算部分
-        // 不分段计算Softplus：y = ln(1 + exp(beta * x)) / beta
-        AscendC::Muls(xLocal, xLocal, static_cast<DTYPE_X>(beta), dataNum);
+            AscendC::Cast(temp1, xLocal, AscendC::RoundMode::CAST_NONE, dataNum);
+            AscendC::CompareScalar(mask, temp1, static_cast<float32_t>(this->threshold), AscendC::CMPMODE::GT, dataNum);
+            AscendC::Muls(temp2, temp1, static_cast<float32_t>(this->beta), dataNum);
+            AscendC::Exp(temp2, temp2, dataNum);
+            AscendC::Adds(temp2, temp2, static_cast<float32_t>(scalar), dataNum);
+            AscendC::Ln(temp2, temp2, dataNum);
+            AscendC::Muls(temp2, temp2, static_cast<float32_t>(1.0f / this->beta), dataNum);
+            AscendC::Muls(temp1, temp1, static_cast<float32_t>(1.0f / this->beta), dataNum);
+            AscendC::Select(temp1, mask, temp1, temp2, AscendC::SELMODE::VSEL_TENSOR_TENSOR_MODE, dataNum);
+            AscendC::Cast(yLocal, temp1, AscendC::RoundMode::CAST_NONE, dataNum);
+        }
+        else
+        {
+            auto temp2 = calBuf2.Get<TYPE_X>(dataNum);
 
-        AscendC::Exp(xLocal, xLocal, dataNum);
+            AscendC::CompareScalar(mask, xLocal, static_cast<TYPE_X>(this->threshold), AscendC::CMPMODE::GT, dataNum);
+            AscendC::Muls(temp2, xLocal, static_cast<TYPE_X>(beta), dataNum);
+            AscendC::Exp(temp2, temp2, dataNum);
+            AscendC::Adds(temp2, temp2, static_cast<TYPE_X>(scalar), dataNum);
+            AscendC::Ln(temp2, temp2, dataNum);
+            AscendC::Muls(temp2, temp2, static_cast<TYPE_X>(1.0f / beta), dataNum);
+            AscendC::Muls(xLocal, xLocal, static_cast<TYPE_X>(1.0f / beta), dataNum);
+            AscendC::Select(yLocal, mask, xLocal, temp2, AscendC::SELMODE::VSEL_TENSOR_TENSOR_MODE, dataNum);
+        }
 
-        AscendC::Adds(xLocal, xLocal, static_cast<DTYPE_X>(scalar), dataNum);
-
-        AscendC::Ln(xLocal, xLocal, dataNum);
-
-        AscendC::Muls(yLocal, xLocal, static_cast<DTYPE_Y>(1.0f / beta), dataNum);
-
-        outQueueY.EnQue<DTYPE_Y>(yLocal);
+        outQueueY.EnQue<TYPE_Y>(yLocal);
         inQueueX.FreeTensor(xLocal);
     }
 
     __aicore__ inline void CopyOut(int32_t progress, uint32_t dataNum)
     {
-        AscendC::LocalTensor<DTYPE_Y> yLocal = outQueueY.DeQue<DTYPE_Y>();
+        AscendC::LocalTensor<TYPE_Y> yLocal = outQueueY.DeQue<TYPE_Y>();
         AscendC::DataCopy(yGm[progress * this->tilingDataNum], yLocal, dataNum);
         outQueueY.FreeTensor(yLocal);
     }
@@ -96,9 +115,9 @@ private:
     AscendC::TQue<AscendC::TPosition::VECIN, PING_PONG_BUFFER_NUM> inQueueX;
     // create queue for output, in this case depth is equal to buffer num
     AscendC::TQue<AscendC::TPosition::VECOUT, PING_PONG_BUFFER_NUM> outQueueY;
-    AscendC::TBuf<AscendC::QuePosition::VECCALC> calBuf;
-    AscendC::GlobalTensor<DTYPE_X> xGm;
-    AscendC::GlobalTensor<DTYPE_Y> yGm;
+    AscendC::TBuf<AscendC::QuePosition::VECCALC> calBuf1, calBuf2, calBuf3;
+    AscendC::GlobalTensor<TYPE_X> xGm;
+    AscendC::GlobalTensor<TYPE_Y> yGm;
 
     uint32_t totalDataNum;
 
@@ -113,7 +132,7 @@ private:
 extern "C" __global__ __aicore__ void softplus(GM_ADDR x, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling)
 {
     GET_TILING_DATA(tiling_data, tiling);
-    KernelSoftplus op;
+    KernelSoftplus<DTYPE_X, DTYPE_Y> op;
     op.Init(x, y, tiling_data.total_data_num,
 
             tiling_data.tiling_loop_num,
